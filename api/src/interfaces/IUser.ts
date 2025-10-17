9d29c92c855f670d6f011aa4cfdda94fbef881c5
sachinthacham <sachinthachamindu26@gmail.com>
2025-10-17 19:28:29 +0530

    update user interface

--- api/src/interfaces/IUser.ts
+++ api/src/interfaces/IUser.ts
@@ -1,12 +1,13 @@
-export interface IUser {
-    _id?: string;
-    name: string;
-    email: string;
-    password?: string;
-    role: "buyer" | "seller" | "admin";
-    googleId?: string;
-    profilePic?: string;
-    createdAt?: Date;
-    updatedAt?: Date;
-  }
-  
+import { Document } from "mongoose";
+
+export interface IUser extends Document {
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
+  role?: string;
+  resetPasswordToken?: string | null;
+  resetPasswordExpires?: Date | null;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+  wishlist: string[];
+}
