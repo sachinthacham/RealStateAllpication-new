64513eaf895aebe9ec27201267c480fff27b9381
sachinthacham <sachinthachamindu26@gmail.com>
2025-10-17 19:28:03 +0530

    implement zod validation file

---
+++ api/src/middlewares/validate.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+import { ZodSchema } from "zod";
+
+export const validate =
+  (schema: ZodSchema<any>) => (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (err: any) {
+      return res.status(400).json({
+        message: "Validation failed",
+        errors: err.errors || err.message,
+      });
+    }
+  };
